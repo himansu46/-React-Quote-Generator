24d909625797a9eb1505fce70c432827f6f6ae18
Himansu Sharma <himansusharma564@gmail.com>
2023-02-02 03:22:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => setQuote(data));
+
+    changeColour();
+  };
+
+  useEffect(() => getQuote(), []);
+  const randomFunc = [
+    "Chocolate",
+    "DarkCyan",
+    "SeaGreen",
+    "gray",
+    "DarkSlateGrey",
+    "Crimson",
+    "Olive",
+    "SaddleBrown",
+    "purple",
+    "teal",
+  ];
+
+  const changeColour = () => {
+    const i = Math.floor(Math.random() * randomFunc.length);
+    const func = randomFunc[i];
+    document.body.style.background = func;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1 style={{ color: "#d3d3d3", textAlign: "center", marginTop: "4%" }}>
+        QuoteGenerator
+      </h1>
+      <div className="innerbox">
+        <h2>"{quote.content}"</h2>
+        <br />
+        <div className="author">
+          <i>~{quote.author}</i>
+        </div>
+
+        <br />
+        <hr></hr>
+        <button
+          onClick={getQuote}
+          class="bg-primary text-white quotebutton"
+          type="submit"
         >
-          Learn React
-        </a>
-      </header>
+          Generate Quote
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
